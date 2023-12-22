fbf9a295b9c37d54e6bfc1b0a65896a4024001ac
JarodEstes <estesjarod48@gmail.com>
2023-12-21 23:53:41 -0600

    <hello.js>

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello, k World! 8 Everybody Dance!")
+console.log('Hello, 8 to the World! boy oh boy')
 
 /*  
 Next, PLAY! 
